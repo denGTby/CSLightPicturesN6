7495d8450915ce340754158a14dfdd704498b06e
dengt_by dengt_by <denis.wse.new@gmail.com>
2023-04-11 13:43:04 +0300

    Добавьте файлы проекта.

---
+++ CSLightPicturesN6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSLightPicturesN6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesPerLine = 7;
+            int totalCountOfPictures = 52;
+            int countFilledRows = totalCountOfPictures / picturesPerLine;
+            int extraCountPictures = totalCountOfPictures % picturesPerLine;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"Количество полностью заполненных рядов картинок: ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($"{countFilledRows}");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"\nКоличество картинок не вошедших в целы ряд: ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($"{extraCountPictures}");
+
+            Console.ReadLine();
+        }
+    }
+}
